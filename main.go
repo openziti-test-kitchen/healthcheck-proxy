15eaf2b60d8cc7e4017ca92fc443ca1956db262f
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-03-25 11:25:07 -0400

    Update main.go

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 )
 
 func main() {
-	idFile := getEnvOrDefault("OPENZITI_HEALTHCHECK_IDENTITY", "/opt/openziti/underlay-host-proxy/identity.json")
+	idFile := getEnvOrDefault("OPENZITI_HEALTHCHECK_IDENTITY", "/opt/openziti/healthcheck/identity.json")
 	port := getEnvOrDefault("OPENZITI_HEALTHCHECK_PROXY_PORT", "2171")
 	allowedPathRegex := getEnvOrDefault("OPENZITI_HEALTHCHECK_ALLOWED_PATH", "^[.*]$")
 	slog.Info("allowed path regex set", "regex", allowedPathRegex)
